18caae16d14a1da152c2a855d173d323d48e066d
phyochan <phyochandeveloper@gmail.com>
2015-09-17 01:55:50 +0630

    Mobile Api

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,9 +3,12 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+use Closure;
 
 class VerifyCsrfToken extends BaseVerifier
 {
+
+    private $openRoutes = ['api/getcount/{id}'];
     /**
      * The URIs that should be excluded from CSRF verification.
      *
@@ -14,4 +17,24 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
+
+
+
+//modify this function
+    public function handle($request, Closure $next)
+    {
+        //add this condition
+        foreach($this->openRoutes as $route) {
+
+            if ($request->is($route)) {
+                return $next($request);
+            }
+        }
+
+        return parent::handle($request, $next);
+    }
+
+
+
+
 }
